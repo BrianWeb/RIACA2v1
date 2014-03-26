db69ae19e9b3e924d5dc35ab168020fc3c7b7173
fredyfontaine <fredyfontaine@yahoo.co.uk>
2014-03-26 01:30:29 +0000

    commit 2

---
+++ RIA-CA2v1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RIA_CA2v1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public string Index()
+        {
+            return "Hello World bbbb";
+        }
+	}
+}
